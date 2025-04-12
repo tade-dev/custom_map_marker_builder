abbb0b597517dc51ff99c74e1dd99583eca7d043
Akintade Oluwaseun <akintadeseun816@gmail.com>
2025-04-12 23:37:26 +0100

    Fix import path and restore custom_marker_builder.dart file

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:custom_marker_builder/custom_map_marker_builder.dart';
+import 'package:custom_marker_builder/custom_marker_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
